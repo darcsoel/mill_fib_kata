b42461e25201718fe77debaf40bad11231b84b59
ross <rostikvunarchuk@gmail.com>
2022-04-17 18:15:10 +0300

    Binet’s formula

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
+from math import sqrt
+
+
+def fibonacci_recursion(n):
+    """
+    Classic solution, recursion overflow possible
+
+    :param n: int - length
+    :return: int - result
+    """
+
+    if n in (0, 1):
+        return n
+
+    if n >= 0:
+        return fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2)
+
+    return fibonacci_recursion(n + 2) - fibonacci_recursion(n + 1)
+
+
+def fibonacci_classic(n):
+    """
+    Classic solution for test positive integers
+
+    :param n:
+    :return:
+    """
+    a, b = 0, 1
+
+    for _ in range(n):
+        a, b = b + a, a
+
+    return a
+
+
 def fib(n):
-    pass
+    """
+    Binet’s formula
+
+    :param n: int - length
+    :return: int - result
+    """
+
+    root_5 = sqrt(5)
+    result = (1 / root_5) * (((1 + root_5) / 2) ** n - ((1 - root_5) / 2) ** n)
+
+    return int(result)
--- tests.py
+++ tests.py
@@ -1,34 +1,48 @@
-from main import fib
+from main import fib, fibonacci_classic
+# from main import fibonacci_recursion as fib
 
 
-def test1():
+def test0():
     assert fib(0) == 0
 
 
-def test2():
+def test1():
     assert fib(1) == 1
 
 
-def test3():
+def test2():
     assert fib(2) == 1
 
 
-def test4():
+def test3():
     assert fib(3) == 2
 
 
-def test5():
+def test4():
     assert fib(4) == 3
 
 
-def test6():
+def test5():
     assert fib(5) == 5
 
 
-def test7():
+def test71():
+    assert fib(71) == 308061521170129
+
+
+def test72():
+    assert fib(72) == 498454011879264
+
+
+def test1000():
     result = 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
     assert fib(1000) == result
 
 
-def test_neg1():
+def test_neg6():
     assert fib(-6) == -8
+
+
+def test_compare_with_classic_solution():
+    for number in range(100):
+        assert fib(number) == fibonacci_classic(number)
